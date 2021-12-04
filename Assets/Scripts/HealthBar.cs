e2bfde76021580bca6af3b3bf7df265d0d9b71a2
Ren Reyes <92892647+todorenki@users.noreply.github.com>
2021-12-04 12:53:37 -0800

    Music + updated health bar
    Updated the health bar using the HealthBar tutorial + added music

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void setHealth(float health)
+    {
+        slider.value = health;
+    }
+   
+}
