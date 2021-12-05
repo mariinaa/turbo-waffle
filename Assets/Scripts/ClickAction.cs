e8ce562d035da0c401a0a5bdca43439fa954659c
mariinaa <89312067+mariinaa@users.noreply.github.com>
2021-12-04 18:03:36 -0800

    Battle MPV complete
    Currently residing in MarinaTest scene

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown()
+    {
+
+        Debug.Log("You've clicked the mouse down");
+
+        if (gameObject.tag == "good")
+        {
+            Debug.Log("you've clicked a good action");
+        }
+
+        else if (gameObject.tag == "neutral")
+        {
+            Debug.Log("You've clicked a neutral action");
+        }
+    }
+}
+
+
