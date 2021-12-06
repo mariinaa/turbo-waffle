f90750da1f1ff72d4abb34650d8b30533754803e
mariinaa <89312067+mariinaa@users.noreply.github.com>
2021-12-06 09:48:51 -0800

    Scene Animations for Win and Lose conditions

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        LoadEndingScene();
+    //    }
+    //}
+
+    public void LoadEndingScene(string winOrLose)
+    {
+        if (winOrLose == "win")
+        {
+            StartCoroutine(LoadEnding("Win")); //need to pass win or lose scene here **********
+        }
+        else if (winOrLose == "lose")
+        {
+            StartCoroutine(LoadEnding("Lose")); //need to pass win or lose scene here **********
+        }
+        
+    }
+
+    IEnumerator LoadEnding(string sceneName)
+    {
+        //play animation
+        transition.SetTrigger("Start"); //name of trigger we created inside of the animator
+
+        //wait for animation  to stop playing
+        yield return new WaitForSeconds(transitionTime);
+
+        //load scene
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
